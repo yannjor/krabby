a4cb2ed42e2977aae7d3f88e55a94fc28de5cdd4
hill <hill@hilll.dev>
2025-03-05 22:06:24 +0000

    provide default config

--- src/lib.rs
+++ src/lib.rs
@@ -57,7 +57,10 @@ pub fn random_pokemon(options: RandomOptions) -> Result<String, Error> {
 }
 
 fn load_db() -> Result<PokemonDatabase, Error> {
-    let config = Config::load()?;
+    let config = Config::load().unwrap_or(Config {
+        language: "en".to_string(),
+        shiny_rate: 1.0 / 32.0,
+    });
     let pokemon_db_file = Asset::get("pokemon.json").expect("Could not read pokemon db file");
     PokemonDatabase::load(&pokemon_db_file, config)
 }
